1012efcc4df69ec7fc87ae30364a591823539b6d
Prabal Manchanda <123109839+Prabal31@users.noreply.github.com>
2024-07-19 14:04:32 -0400

    Had all necessary Imports

--- main.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Prabh Manchanda on 2024-07-19.
-//
-
-#include "main.h"
---
+++ main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Prabh Manchanda on 2024-07-19.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
